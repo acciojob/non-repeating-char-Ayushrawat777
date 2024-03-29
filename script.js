b648ae9e83e60686f3df9ec62e66e44d40570c23
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-03-29 14:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
-}
+
+function firstNonRepeatedChar(string) {
+        let index = -1;
+        let fnc = " ";
+
+        if (string.length == 0) {
+        return "EMPTY STRING"
+        }
+
+        for (let i of string) {
+          if (string.split(i).length - 1 === 1) {
+            fnc = i;
+            break;
+          } else {
+            index += 1;
+          }
+        }
+        if (index === string.length - 1) {
+          return 'Null';
+        } else {
+          return fnc;
+        }
+      }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+     
